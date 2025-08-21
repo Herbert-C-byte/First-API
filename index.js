68c37a65addf91582b204760850552995916ed0f
Herbert-C-byte <epsylon344@gmail.com>
2025-08-21 17:19:55 +0100

    async/await

--- index.js
+++ index.js
@@ -1,10 +1,24 @@
-fetch('https://official-joke-api.appspot.com/random_joke')
-.then(data => data.json())
-.then(response => {
-  console.log("Joke Id:", response.id);
-  console.log("Setup: ", response.setup);
-  console.log("Punchline: ", response.punchline);
-})
-.catch(error => {
-  console.log("Something went wrong bro: ", error);
-})
+async function jokeAPI() {
+  try {
+    const data = await fetch('https://official-joke-api.appspot.com/random_joke');
+    const joke = await data.json();
+
+    console.log("Setup: ", joke.setup);
+    console.log("Punchline: ", joke.punchline);
+  } catch (error) {
+    console.log("Something went wrong:", error);
+  }
+}
+
+jokeAPI();
+
+// fetch('https://official-joke-api.appspot.com/random_joke')
+// .then(data => data.json())
+// .then(response => {
+//   console.log("Joke Id:", response.id);
+//   console.log("Setup: ", response.setup);
+//   console.log("Punchline: ", response.punchline);
+// })
+// .catch(error => {
+//   console.log("Something went wrong bro: ", error);
+// })

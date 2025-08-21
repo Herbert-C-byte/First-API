bb47e275e71d4f44955aae2683880b62221c72ef
Herbert-C-byte <epsylon344@gmail.com>
2025-08-21 17:09:33 +0100

    First commit: joke

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-async
+fetch('https://official-joke-api.appspot.com/random_joke')
+.then(data => data.json())
+.then(response => {
+  console.log("Joke Id:", response.id);
+  console.log("Setup: ", response.setup);
+  console.log("Punchline: ", response.punchline);
+})
+.catch(error => {
+  console.log("Something went wrong bro: ", error);
+})

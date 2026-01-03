6613347c4b7c99ff05d36c9685bfd9d8b99d8c2a
Herbert-C-byte <epsylon344@gmail.com>
2026-01-03 23:58:55 +0100

    Uncommited the fetch

--- index.js
+++ index.js
@@ -12,13 +12,14 @@ async function jokeAPI() {
 
 jokeAPI();
 console.log("This is after the jokeAPI call.");
-// fetch('https://official-joke-api.appspot.com/random_joke')
-// .then(data => data.json())
-// .then(response => {
-//   console.log("Joke Id:", response.id);
-//   console.log("Setup: ", response.setup);
-//   console.log("Punchline: ", response.punchline);
-// })
-// .catch(error => {
-//   console.log("Something went wrong bro: ", error);
-// })
+
+fetch('https://official-joke-api.appspot.com/random_joke')
+.then(data => data.json())
+.then(response => {
+  console.log("Joke Id:", response.id);
+  console.log("Setup: ", response.setup);
+  console.log("Punchline: ", response.punchline);
+})
+.catch(error => {
+  console.log("Something went wrong bro: ", error);
+})
